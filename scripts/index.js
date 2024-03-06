69e5371de497e74c599713ae85fbd0b4d39cea11
Fednev Ruslan <fednevr97@yandex.ru>
2024-03-07 00:22:29 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
 // @todo: Темплейт карточки
-
+const templateCards = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardsContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(link, name) {
+    const cardElement = templateCards.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    const cardDelete = cardElement.querySelector('.card__delete-button');
+
+    cardDelete.addEventListener('click', deleteButton);
+
+    return cardElement;
+};
+
+function addCard (cardElement) {
+    cardsContainer.append(cardElement);
+}
+
 
 // @todo: Функция удаления карточки
 
+function deleteButton(event) {
+    event.target.closest('.places__item').remove();
+}
 // @todo: Вывести карточки на страницу
+
+function renderCards() {
+    initialCards.forEach(function(item) {
+        const card = createCard(item.link, item.name, deleteButton);
+        addCard(card);
+    });
+  };
+  
+  // Вызов функции renderCards при загрузке страницы
+  document.addEventListener('DOMContentLoaded', renderCards);
+
